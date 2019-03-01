891e804f8b42d06c6ede5295913b3e29575cb4a5
Julia Romanowski <juliaromanowski@gmail.com>
2019-02-28 19:04:45 -0500

    posenet tests

--- src/App.js
+++ src/App.js
@@ -7,16 +7,34 @@ class App extends Component {
     this.state = {
       net: {}
     }
+    this.getImagePosition = this.getImagePosition.bind(this);
+  }
+  async getImagePosition(image) {
+    var imageScaleFactor = 0.5;
+    var outputStride = 16;
+    var flipHorizontal = false;
+    console.log("Posnet gonna load");
+    const net = posenet.load().then(whatever => {
+      console.log("Posnet loaded");
+    });
+    //const pose = await net.estimateSinglePose(image, imageScaleFactor, flipHorizontal, outputStride);
+    //console.log(pose)
   }
 
-  async componentDidMount() {
-    const net = await posenet.load();
-    this.setState({ net })
+  componentDidMount() {
+    console.log("did mount")
+    var imageElement = document.getElementById('img1');
+    console.log(imageElement)
+    this.getImagePosition(imageElement)
   }
 
   render() {
-    console.log(posenet)
-    return (<div> root</div>);
+    return (
+      <div>
+        <h1>Jam Cam</h1>
+        <img id='img1' src='/images/anatomy_287_3321_bjkforsacrum.jpg' alt="yoga" />
+      </div>
+    );
   }
 }
 export default App;
