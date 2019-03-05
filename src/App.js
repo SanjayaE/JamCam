85642f052206c1389695787fa9f289da4491be02
pigbog <mesteves@gmail.com>
2019-03-05 14:51:00 -0500

    added keyboard buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import capture from "./services/capture.js";
+import React, { Component } from 'react';
+import capture from './services/capture.js';
 // import * as posenet from '@tensorflow-models/posenet';
 // const context = new AudioContext();
 //const Tone = require("tone");
@@ -9,11 +9,18 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.video = {};
-    this.state = {};
+    this.state = {
+      noteC: 'inactive',
+      noteD: 'inactive',
+      noteE: 'inactive',
+      noteF: 'inactive',
+      noteA: 'inactive',
+      noteB: 'inactive'
+    };
   }
 
   componentDidMount = async () => {
-    console.log("did mount");
+    console.log('did mount');
     capture();
   };
 
@@ -21,9 +28,19 @@ class App extends Component {
     return (
       <div className="container">
         <h1>Jam Cam</h1>
-        <video id="video" width="640" height="480" controls autoPlay />
-        <canvas id="overlay" />
 
+        <div id="keyboard_container">
+          <div className="keyboard_C">C</div>
+          <div className="keyboard_D">D</div>
+          <div className="keyboard_E">E</div>
+          <div className="keyboard_F">F</div>
+          <div className="keyboard_G">G</div>
+          <div className="keyboard_A">A</div>
+          <div className="keyboard_B">B</div>
+
+          <video id="video" width="640" height="480" controls autoPlay />
+          <canvas id="overlay" />
+        </div>
         {/* <div id="overlay_container">
           <canvas id="overlay" height="300" width="300" />
 
