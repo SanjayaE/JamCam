f79ab9a60fa16c23ddf0e81bb6ff1e8b5e69d1da
Julia Romanowski <juliaromanowski@gmail.com>
2019-03-08 13:35:06 -0500

    key naming fix

--- src/services/keyboard2.js
+++ src/services/keyboard2.js
@@ -14,30 +14,30 @@ const keyboard = async (pose, cb) => {
     };
 
     if (withinKB() && Y <= 80) {
-        console.log('mega_chord1');
-        cb('mega_chord1');
+        console.log('a2');
+        cb('a2');
         // TODO: Update Color of the C key on the DOM
     } else if (withinKB() && Y <= 160 && Y >= 81) {
-        console.log('mega_chord2');
-        cb('mega_chord2');
+        console.log('b2');
+        cb('b2');
     } else if (withinKB() && Y <= 240 && Y >= 161) {
-        console.log('mega_chord3');
-        cb('mega_chord3');
+        console.log('c3');
+        cb('c3');
     } else if (withinKB() && Y <= 320 && Y >= 241) {
-        console.log('mega_chord4');
-        cb('mega_chord4');
+        console.log('d3');
+        cb('d3');
     } else if (withinKB() && Y <= 400 && Y >= 321) {
-        console.log('mega_chord5');
-        cb('mega_chord4');
+        console.log('e3');
+        cb('e3');
     } else if (withinKB() && Y <= 480 && Y >= 401) {
-        console.log('mega_chord6');
-        cb('mega_chord4');
+        console.log('f3');
+        cb('f3');
     } else if (withinKB() && Y <= 560 && Y >= 481) {
-        console.log('mega_chord7');
-        cb('mega_chord4');
+        console.log('g3');
+        cb('g3');
     } else if (withinKB() && Y <= 640 && Y >= 561) {
-        console.log('mega_chord8');
-        cb('mega_chord4');
+        console.log('a3');
+        cb('a3');
     } else {
         // console.log('none');
         cb('none');
