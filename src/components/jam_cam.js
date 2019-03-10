9b7bf348db2e34f07ed36b7a44800b1169d2c412
pigbog <mesteves@gmail.com>
2019-03-10 17:37:55 -0400

    Added toggleMode logic to panel.js and interactive_window.js

--- src/components/jam_cam.js
+++ src/components/jam_cam.js
@@ -6,56 +6,68 @@ import capture from '../services/capture';
 import { CameraStart, CameraStop } from '../services/camera';
 
 class JamCam extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bodyPartLocation: {
+        leftWrist: {
+          x: 0,
+          y: 0
+        },
+        rightWrist: {
+          x: 0,
+          y: 0
+        }
+      },
+      mode: 1
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            bodyPartLocation: {
-                leftWrist: {
-                    x: 0,
-                    y: 0
-                },
-                rightWrist: {
-                    x: 0,
-                    y: 0
-                }
-            },
-            mode: 1,
-        };
+  componentDidMount = async () => {
+    //Start Camera
+    CameraStart();
+    //Start Capture and Provide Callback
+    capture(this.receiveNewBodyPartLocation);
+  };
+
+  componentWillUnmount = () => {
+    //turn off camera and audio when you switch from the video page
+    CameraStop();
+    stopAudio();
+  };
+
+  //Takes in body part locations and maps to keyboard and loops
+  receiveNewBodyPartLocation = bodyPartLocation => {
+    this.setState({
+      bodyPartLocation
+    });
+  };
+
+  toggleMode = () => {
+    let mode;
+    console.log('Mode is: ', this.state.mode);
+    if (this.state.mode === 1) {
+      mode = 2;
+    } else {
+      mode = 1;
     }
+    this.setState({ mode });
+  };
 
-    componentDidMount = async () => {
-        //Start Camera
-        CameraStart();
-        //Start Capture and Provide Callback
-        capture(this.receiveNewBodyPartLocation);
-    };
-
-    componentWillUnmount = () => {
-        //turn off camera and audio when you switch from the video page
-        CameraStop();
-        stopAudio();
-    };
-
-    //Takes in body part locations and maps to keyboard and loops
-    receiveNewBodyPartLocation = (bodyPartLocation) => {
-        this.setState(
-            {
-                bodyPartLocation
-            }
-        );
-    };
-
-    render() {
-        return (
-            <div className="container">
-                < InteractiveWindow leftWrist={this.state.bodyPartLocation.leftWrist} rightWrist={this.state.bodyPartLocation.rightWrist} />
-                <video id="video" width="640" height="480" controls autoPlay />
-                <canvas id="overlay" />
-                < Panel />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="container">
+        <InteractiveWindow
+          leftWrist={this.state.bodyPartLocation.leftWrist}
+          rightWrist={this.state.bodyPartLocation.rightWrist}
+          mode={this.state.mode}
+        />
+        <video id="video" width="640" height="480" controls autoPlay />
+        <canvas id="overlay" />
+        <Panel mode={this.state.mode} toggleMode={this.toggleMode} />
+      </div>
+    );
+  }
 }
 
 export default JamCam;
